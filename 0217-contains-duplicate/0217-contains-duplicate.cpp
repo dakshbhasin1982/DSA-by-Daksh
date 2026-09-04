b8179d7682948e3f849d9adbd03ab57806406734
dakshbhasin1982 <dakshbhasin1982@gmail.com>
2026-09-04 13:15:14 +0530

    Time: 27 ms (87.01%), Space: 82.5 MB (99.64%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,9 +3,9 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
 
-        for(int i=1; i<nums.size(); i++){
+        for(int i=1; i<nums.size() ; i++){
             if(nums[i]==nums[i-1]){
-                return true;
+            return true;
             }
         }
         return false;
