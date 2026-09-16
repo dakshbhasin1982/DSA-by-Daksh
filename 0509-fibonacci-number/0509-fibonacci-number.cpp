3b39a19bc0e1e385de078811d81fe1a2da9521b4
dakshbhasin1982 <dakshbhasin1982@gmail.com>
2026-09-16 23:24:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
-    int fib(int n) {
+    
+    int solmemo(int n,vector<int>& dp) {
        if(n==0 || n==1){
         return n;
        } 
-       return fib(n-1)+fib(n-2);
-       
+       if(dp[n]!=-1){
+         return dp[n];
+       }
+
+       return dp[n] = solmemo(n-1,dp)+solmemo(n-2,dp);
+
+    }
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return solmemo(n,dp);
     }
 };
